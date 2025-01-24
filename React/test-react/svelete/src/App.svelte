bd9074b3e5a34877a476549beef5ed536d701fc3
Ankit7405 <ankitmalviya8601@gmail.com>
2025-01-24 16:05:14 +0530

    Cart application

---
+++ React/test-react/svelete/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Table from "./Table.svelte";
+
+  let colum = [
+      { label: "Entity", prop: "entity" },
+      { label: "GBGF", prop: "gbgf" }
+  ];
+
+  let dataObj = [
+      {
+          tenentInfo: {
+              entity: "BH_BBME",
+              gbgf: "WPB"
+          }
+      },
+      {
+          tenentInfo: {
+              entity: "AE_BBME",
+              gbgf: "WPB"
+          }
+      }
+  ];
+</script>
+
+<Table {dataObj} {colum} />
